170822ab2ea6b19fe0e41dff6c134b6959d26124
Geli1986 <angel.bajo.sanchez@gmail.com>
2026-07-12 16:30:53 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+/* ======================================
+   BSGA CONSULTING
+====================================== */
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const elements = document.querySelectorAll(
+        ".service-card, .about-box, .timeline-item, .industry-grid div, .stat"
+    );
+
+    const observer = new IntersectionObserver((entries)=>{
+
+        entries.forEach(entry=>{
+
+            if(entry.isIntersecting){
+
+                entry.target.classList.add("show");
+
+            }
+
+        });
+
+    },{
+
+        threshold:0.15
+
+    });
+
+    elements.forEach(el=>observer.observe(el));
+
+});
